cfdd74def6e09a43a6263dddea31e1c63a411929
Gustavo Todeschini <gutodeschinii@outlook.com>
2024-04-16 21:55:26 -0300

    Aprendendo mais sobre objetos com js

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const carro = {
+    marca:"ford", 
+    modelo: "ka", 
+    ano:2015, 
+    placa:"ABC-1234",
+    buzina: function() {alert('Biiiiiiiiiiiiiiiii')},
+    completo: function(){
+        return "A marca é "+this.marca+" e o modelo é: "+this.modelo+" .";
+    }
+};
+
+console.log(carro.completo());
